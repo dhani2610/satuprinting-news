d7f17a61d475f46cecd589a2642e4d01dd871603
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-05-10 11:12:00 +0700

    fix : update orderby mesin

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -61,7 +61,7 @@ class FrontendController extends Controller
         $data['allProducts'] = Services::whereNull('deleted_at')->where('status',1)->orderBy('created_at','desc')->limit(8)->get();
         $data['allProductsDisc'] = Services::whereNull('deleted_at')->where('status',1)->where('is_diskon',1)->orderBy('created_at','desc')->limit(8)->get();
         $data['klien'] = Klien::orderBy('created_at','desc')->get();
-        $data['mesin'] = Mesin::orderBy('created_at','desc')->get();
+        $data['mesin'] = Mesin::orderBy('created_at','asc')->get();
         $data['portofolio'] = Portofolio::orderBy('created_at','desc')->get();
         // return response()->json($data['mesin']);
         return view('landing.home.index',$data);
